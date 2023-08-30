0d122e12ea1941155c1297a5fb5da83dfdc83691
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-08-30 08:49:25 -0300

    :zap: Perf: Melhorando a validações para quando uma promessa for feita

--- src/controllers/crowdfunding.js
+++ src/controllers/crowdfunding.js
@@ -1,4 +1,5 @@
 const conexao = require("../connection");
+const schemaFulFillPromise = require("../validations/shecmaFulFillPromise");
 
 const getProduct = async (req, res) => {
   const { idProduto } = req.params;
@@ -36,48 +37,47 @@ const catchPlans = async (req, res) => {
 
 const fulfillPromise = async (req, res) => {
   const { id_plano, valor } = req.body;
-  if (!id_plano || !valor) {
-    return res.status(400).json({ error: "Parâmetros ausentes" });
-  }
-  if (typeof valor === "string") {
+
+  try {
+    await schemaFulFillPromise.validate(req.body);
     const clearValue = valor.replace(",", "").replace(".", "");
     const newValue = Number(clearValue);
-
-    try {
-      await conexao("apoios").insert({ valor: newValue, id_plano });
-      const { quantidade } = await conexao("planos")
-        .select("quantidade")
-        .where("id", id_plano)
-        .first();
-      await conexao("planos")
-        .update({ quantidade: quantidade - 1 })
-        .where("id", id_plano);
-
-      const { id_produto } = await conexao("planos")
-        .select("id_produto")
-        .where("id", id_plano)
-        .first();
-
-      const { valor_arrecadado, total_apoios } = await conexao("produtos")
-        .select("valor_arrecadado", "total_apoios")
-        .where("id", id_produto)
-        .first();
-
-      const produto = await conexao("produtos")
-        .update({
-          valor_arrecadado: valor_arrecadado + newValue,
-          total_apoios: total_apoios + 1,
-        })
-        .where("id", id_produto);
-
-      res.status(201).json("Apoio realizado com sucesso");
-    } catch (error) {
-      res.status(500).json({ error: `${error?.message}` });
+    if (!newValue) {
+      return res
+        .status(404)
+        .json({
+          error: "O valor deve ser mandando em centavos no tipo string",
+        });
     }
-  } else {
-    return res
-      .status(400)
-      .json({ error: "Valor precisa ser passada como string em centavos" });
+    await conexao("apoios").insert({ valor: newValue, id_plano });
+    const { quantidade } = await conexao("planos")
+      .select("quantidade")
+      .where("id", id_plano)
+      .first();
+    await conexao("planos")
+      .update({ quantidade: quantidade - 1 })
+      .where("id", id_plano);
+
+    const { id_produto } = await conexao("planos")
+      .select("id_produto")
+      .where("id", id_plano)
+      .first();
+
+    const { valor_arrecadado, total_apoios } = await conexao("produtos")
+      .select("valor_arrecadado", "total_apoios")
+      .where("id", id_produto)
+      .first();
+
+    const produto = await conexao("produtos")
+      .update({
+        valor_arrecadado: valor_arrecadado + newValue,
+        total_apoios: total_apoios + 1,
+      })
+      .where("id", id_produto);
+
+    res.status(201).json("Apoio realizado com sucesso");
+  } catch (error) {
+    res.status(500).json({ error: `${error?.message}` });
   }
 };
 
---
+++ src/validations/shecmaFulFillPromise.js
@@ -0,0 +1,8 @@
+const yup = require("./settings");
+
+const schemaFulFillPromise = yup.object().shape({
+  id_plano: yup.number().required("O campo Id do plano é necessário"),
+  valor: yup.string().strict().required("O valor é um campo obrigatório"),
+});
+
+module.exports = schemaFulFillPromise;
